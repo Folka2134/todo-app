8082488d2e1cd339394f58a7609a786d689e156a
Folka2134 <folka2134@gmail.com>
2022-02-02 13:36:26 +0000

    Setup basic router file

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+const router = require('./routes/todos')
+
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req,res) => res.send('test'))
+app.use(router)
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT:${PORT}`))

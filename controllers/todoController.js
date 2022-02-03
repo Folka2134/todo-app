0b141c57c87a65fce836c20c391a2991481c3384
Folka2134 <folka2134@gmail.com>
2022-02-03 13:14:46 +0000

    Updated controllers to now interract with data sent to database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,12 +1,46 @@
+const TodosModel = require('../models/TodosModel')
+
 //@desc GET Get all Todos
 //@route GET /
-exports.getTodos = (req, res, next) => {
-  res.send('GET Todos')
+exports.getTodos = async (req, res, next) => {
+  try {
+    const todos = await TodosModel.find()
+    return res.status(200).json({
+      success: true,
+      data: todos
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: 'Server error'
+    })
+  }
 }
 //@desc POST Add new Todo
 //@route POST /
-exports.addTodo = (req, res, next) => {
-  res.send('POST Todos')
+exports.addTodo = async (req, res, next) => {
+  try {
+    const { todo } = req.body
+    const newTodo = await TodosModel.create(req.body)
+
+    return res.status(201).json({
+      success: true,
+      data: newTodo
+    })
+  } catch (err) {
+    if (err.name === 'ValidateionError') {
+      const messages = Object.values(err.errors.map((val) => val.message))
+      return res.status(400).json({
+        success: false,
+        error: messages
+      })
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error'
+      })
+    }
+  }
 }
 //@desc DELETE Delete Todo
 //@route DELETE /:id

b951151b45227ef7aa8095c1554e69b000dcbe14
Folka2134 <folka2134@gmail.com>
2022-02-17 12:37:25 +0000

    Clean template

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,7 @@
-import { Input } from './components/Input';
-import { List } from './components/List';
-
-import { GlobalProvider } from './context/GlobalState';
-
-import './App.css';
-
 function App() {
   return (
-    <div className='bg-[url("./images/blob-background.svg")] bg-cover grid h-screen content-center'>
-      <div className='grid justify-center'>
-        <GlobalProvider>
-          <Input />
-          <List />
-          <p>test</p>
-        </GlobalProvider>
-      </div>
+    <div>
+      new
     </div>
   );
 }
